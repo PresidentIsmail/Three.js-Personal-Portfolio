c1258c17329d14f66b34a332d5bf5215c608467a
Ismail <shaikhnagismail@gmail.com>
2023-08-07 12:37:55 +0200

    autorotate computer on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,31 +5,31 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative mx-auto h-screen w-full`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="mt-5 flex flex-col items-center justify-center">
+          <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
+          <div className="violet-gradient h-40 w-1 sm:h-80" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Ismail</span>
+            Hi, I'm <span className="text-[#915EFF]">Ismail</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces <br className='sm:block hidden' />
-             and web applications
+            I develop user interfaces <br className="hidden sm:block" />
+            and web applications
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute bottom-4 z-10 flex w-full items-center justify-center  ">
+        <a href="#about" className="flex flex-col items-center">
+          <div className="flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-secondary p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -39,9 +39,11 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="mb-1 h-3 w-3 rounded-full bg-secondary"
             />
           </div>
+
+          <span className="text-white lg:hidden">Click To Scroll</span>
         </a>
       </div>
     </section>
